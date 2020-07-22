810f3f3a6744e5beca880c3dea120e924525ea02
HeeWung Song <heewungsong@HeeWungs-MacBook-Pro.local>
2020-07-22 12:26:23 +0900

    project scaffold

---
+++ mongo/connect.js
@@ -0,0 +1,30 @@
+
+import mongoose from "mongoose"
+
+export const connect = async () => {
+  const { DB_HOST, DB_NAME } = process.env
+  try {
+    mongoose.set("useFindAndModify", false)
+    await mongoose.connect(`mongodb+srv://${DB_HOST}/${DB_NAME}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+
+    mongoose.connection.on("error", function (err) {
+      console.error(err)
+    })
+
+    mongoose.connection.on("disconnected", async () => {
+      console.log("Mongoose disconnected")
+    })
+
+    //mongoose.set("useCreateIndex", true)
+  } catch (e) {
+    throw e
+  }
+}
+
+export const disConnect = () => {
+  mongoose.disconnect()
+}
